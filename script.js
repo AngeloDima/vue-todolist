ba3bb233d8f8d7debc71a540d8e5c68c6ed212da
Angelo <Angelodimauro91@gmail.com>
2022-09-14 16:18:57 +0200

    correzione codice

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const caffè = {
-    tipo: `borbone`,
-    colore : `nero`
-};
+// const caffè = {
+//     tipo: `borbone`,
+//     colore : `nero`
+// };
 
-const sale = {
-    tipo: `ingrediente`,
-    colore: `bianco`
-};
+// const sale = {
+//     tipo: `ingrediente`,
+//     colore: `bianco`
+// };
 
-const provoletta = {
-    tipo: `cibo`,
-    colore: `rosa`
-};
+// const provoletta = {
+//     tipo: `cibo`,
+//     colore: `rosa`
+// };
 
-const stitch = {
-    tipo: `alieno`,
-    colore: `azzurro/blu`
-};
+// const stitch = {
+//     tipo: `alieno`,
+//     colore: `azzurro/blu`
+// };
 
 
 const app = new Vue (
@@ -24,11 +24,34 @@ const app = new Vue (
         el: `#root`,
         data: {
             Elementi : [
-                `caffe`,
-                `sale`,
-                `provoletta`,
-                `stitch`
+                {
+                    item: `caffè`,
+                    tipo: `borbone`,
+                    todo: true
+                },
+
+                {
+                    item: `canguro`,
+                    tipo: `animale`,
+                    todo: false
+                },
+
+                {
+                    item: `pizza`,
+                    tipo: `Cibo`,
+                    todo: true
+                },
+
+                {
+                    item: `Stitch`,
+                    tipo: `Alieno`,
+                    todo: false
+                },
             ]
-        }
+
+
+
+
+        }       
     }
-)
+)    
