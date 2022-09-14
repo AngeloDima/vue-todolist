5f3bf3e04b93ea3f9b2b52a9ffaf909d39c52339
Angelo <Angelodimauro91@gmail.com>
2022-09-14 23:37:54 +0200

    bloccato

--- script.js
+++ script.js
@@ -1,57 +1,38 @@
-// const caffè = {
-//     tipo: `borbone`,
-//     colore : `nero`
-// };
-
-// const sale = {
-//     tipo: `ingrediente`,
-//     colore: `bianco`
-// };
-
-// const provoletta = {
-//     tipo: `cibo`,
-//     colore: `rosa`
-// };
-
-// const stitch = {
-//     tipo: `alieno`,
-//     colore: `azzurro/blu`
-// };
 
 
-const app = new Vue (
-    {
-        el: `#root`,
-        data: {
-            Elementi : [
-                {
-                    item: `caffè`,
-                    tipo: `borbone`,
-                    todo: true
-                },
+ const app = new Vue (
+     {
+         el: `#root`,
+         data: {
+             Elementi : [
+                 {
+                     item: "caffè",
+                     todo: true,
+                 },
 
-                {
-                    item: `canguro`,
-                    tipo: `animale`,
-                    todo: false
-                },
+                 {
+                     item: "canguro",
+                     todo: true,
+                 },
 
-                {
-                    item: `pizza`,
-                    tipo: `Cibo`,
-                    todo: true
-                },
+                 {
+                     item: "pizza",  
+                     todo: true,
+                 },
 
-                {
-                    item: `Stitch`,
-                    tipo: `Alieno`,
-                    todo: false
-                },
-            ]
+                 {
+                     item: "Stitch",                   
+                     todo: true,
+                 },
+             ],
+
+             doneList : [``],
+        },
+        
+        
+     }
+ )    
 
 
 
 
-        }       
-    }
-)    
