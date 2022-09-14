155f83283f1bd085f49a274bf4508149fa553b38
Angelo <Angelodimauro91@gmail.com>
2022-09-14 15:10:03 +0200

    Elementi js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+const Elementi = [
+    {
+        
+        text: 'caffè Nero',
+    },
+    {
+        text: 'caffè Rosso',
+    },
+    {
+        text: 'caffè Verde',
+    {
+        text: 'caffè brutto',
+    },
+    {
+        text: 'caffè bello',
+    }
+];
+
+
+
+
 const app = new vue (
     {
         el: `List`,
         data: {
-            
+
         }
     }
 )
