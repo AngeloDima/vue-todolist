388a172faa62709c3d6a69fa0692656d2b918236
Angelo <Angelodimauro91@gmail.com>
2022-09-14 15:46:38 +0200

    js Aggiunta degli aggiunta attributi negli oggetti

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+const caffè = {
+    tipo: `borbone`,
+    colore : `nero`
+};
 
+const sale = {
+    tipo: `ingrediente`,
+    colore: `bianco`
+};
 
+const provoletta = {
+    tipo: `cibo`,
+    colore: `rosa`
+};
+
+const stitch = {
+    tipo: `alieno`,
+    colore: `azzurro/blu`
+};
 
 
 const app = new Vue (
@@ -9,7 +26,8 @@ const app = new Vue (
             Elementi : [
                 `caffe`,
                 `sale`,
-                `provoletta`
+                `provoletta`,
+                `stitch`
             ]
         }
     }
